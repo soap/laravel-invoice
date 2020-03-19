50632aad487d5a4037535a2b78cf073be0b0a7a4
kablanfatih <kablanfatih34@gmail.com>
2020-03-19 16:03:07 +0300

    base currency changed

--- config/invoicable.php
+++ config/invoicable.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'default_currency' => 'EUR',
+    'default_currency' => 'TRY',
     'default_status' => 'concept',
-    'locale' => 'nl_NL',
+    'locale' => 'tr_TR',
     'table_names' => [
         'invoices' => 'invoices',
         'invoice_lines' => 'invoice_lines',
